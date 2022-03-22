5c2cf7d4d9c8493f15ccad1a77687a34dcc59f36
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-22 11:49:17 -0700

    render function created

--- render-utils.js
+++ render-utils.js
@@ -1,3 +1,28 @@
-import { fetchAllGemstonez } from "./fetch-utils.js";
+import { fetchAllGemstonez } from './fetch-utils.js';
 
-function renderAllGemstonez
+export function renderGemstonez(gem) {
+    const gemstoneEl = document.createElement('div');
+    const nameEl = document.createElement('h2');
+    const varietyEl = document.createElement('p');
+    const colorEl = document.createElement('p');
+    const propertiesEl = document.createElement('ul');
+
+    gemstoneEl.classList.add('gem');
+
+    nameEl.textContent = gem.name;
+    varietyEl.textContent = gem.variety;
+    colorEl.textContent = gem.color;
+    propertiesEl.textContent = gem.uses;
+
+    for (let property of propertiesEl) {
+        const propertyEl = document.createElement('li');
+
+        propertyEl.textContent = property;
+
+        propertiesEl.appendChild(propertyEl);
+    }
+
+    gemstoneEl.append(nameEl, varietyEl, colorEl, propertiesEl);
+
+    return gemstoneEl;
+}
