99efe8802e1411ce835f4ef1bc16ef977cc7ef19
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-22 13:57:51 -0700

    styling for first table

--- render-utils.js
+++ render-utils.js
@@ -2,7 +2,7 @@
 
 export function renderGemstonez(gem) {
     const gemstoneEl = document.createElement('div');
-    const nameEl = document.createElement('h2');
+    const nameEl = document.createElement('h4');
     const varietyEl = document.createElement('p');
     const colorEl = document.createElement('p');
     const propertiesEl = document.createElement('ul');
